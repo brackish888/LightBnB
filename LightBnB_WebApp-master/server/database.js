b04a003d9158ad832fa4c7cde9389987fa5d3af3
Sam T <bbrackish@protonmail.com>
2021-04-02 02:43:55 +0000

    add getAllReservations sql query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'vagrant',
-  password: '123',
+  password: '1234',
   host: 'localhost',
   database: 'lightbnb'
 });
@@ -64,7 +64,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, 
+  reservations.*, 
+  avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then(res => { return (res.rows) });
 }
 exports.getAllReservations = getAllReservations;
 
